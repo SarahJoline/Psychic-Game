856391cd1666b80aea6c18680ee31fabd6125299
Sarah Naas <sjnaas@gmail.com>
2019-10-15 18:30:12 -0700

    went over & fixed my JS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,34 +1,50 @@
 var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var lives = 9;
 var wins = 0;
 var losses = 0;
-var lettersGuessed = []
-var userGuess;
 
-winning = document.getElementById("winning");
-winning.innerHTML = 'Wins: ' + wins;
+restart();
 
-losing = document.getElementById("losing");
-losing.innerHTML = 'Losses: ' + losses;
-
-guesses = document.getElementById("guesses");
-guesses.innerHTML = 'Guesses Remaining: ' + lives;
+function restart() {
+    var lives = 9;
+    var alreadyGuessed = [];
+    var letter = alphabet[Math.floor(Math.random() * alphabet.length)];
+    console.log(letter);
 
 
-var letter = alphabet[Math.floor(Math.random() * alphabet.length)];
 
-function lettersGuessed() {
+    document.onkeyup = function (event) {
+        var userGuess = event.key;
+        var noDoubles = alreadyGuessed.includes(userGuess);
+        var validKeys = alphabet.includes(userGuess);
+        //How would I write this if I don't want to use alerts? I just want the game to wait for 
+        //a valid button to be pressed.
+        if (noDoubles) {
+            alert('Letter already guessed');
+        } else if (!validKeys) {
+            alert('Not a letter');
+        } else {
+            game();
+        }
 
-    document.querySelector('#guessed').innerHTML = "Your Guesses so far: " + guessed.join(', ');
-};
-
-document.onkeyup = function (event) {
-    var userGuess = event.key;
-
-    if (userGuess === letter) {
-        wins++;
-    } else {
-        lettersGuessed.push(userGuess);
-        document.getElementById('#guessed').innerHTML = lettersGuessed;
+        function game() {
+            if (userGuess === letter) {
+                wins++;
+                document.querySelector('#winning').innerHTML = wins;
+                restart();
+            } else {
+                lives--;
+                document.querySelector('#guesses').innerHTML = lives;
+                alreadyGuessed.push(userGuess);
+                document.querySelector('#guessed').innerHTML = alreadyGuessed.join(', ');
+            }
+            if (lives === 0) {
+                losses++;
+                document.querySelector('#losing').innerHTML = losses;
+                restart();
+            }
+        }
     }
-};
+}
+
+
+// when lives at 0 -- game restarts.
