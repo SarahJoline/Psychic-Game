b9ff39accc7bc4566997e71c30de53051b3ab3b2
Sarah Naas <sjnaas@gmail.com>
2019-10-16 01:13:39 -0700

    added CSS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,50 +1,74 @@
-var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var alphabet = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z"
+];
 var wins = 0;
 var losses = 0;
 
 restart();
 
 function restart() {
-    var lives = 9;
-    var alreadyGuessed = [];
-    var letter = alphabet[Math.floor(Math.random() * alphabet.length)];
-    console.log(letter);
+  var lives = 9;
+  var alreadyGuessed = [];
+  var letter = alphabet[Math.floor(Math.random() * alphabet.length)];
+  console.log(letter);
 
-
-
-    document.onkeyup = function (event) {
-        var userGuess = event.key;
-        var noDoubles = alreadyGuessed.includes(userGuess);
-        var validKeys = alphabet.includes(userGuess);
-        //How would I write this if I don't want to use alerts? I just want the game to wait for 
-        //a valid button to be pressed.
-        if (noDoubles) {
-            alert('Letter already guessed');
-        } else if (!validKeys) {
-            alert('Not a letter');
-        } else {
-            game();
-        }
-
-        function game() {
-            if (userGuess === letter) {
-                wins++;
-                document.querySelector('#winning').innerHTML = wins;
-                restart();
-            } else {
-                lives--;
-                document.querySelector('#guesses').innerHTML = lives;
-                alreadyGuessed.push(userGuess);
-                document.querySelector('#guessed').innerHTML = alreadyGuessed.join(', ');
-            }
-            if (lives === 0) {
-                losses++;
-                document.querySelector('#losing').innerHTML = losses;
-                restart();
-            }
-        }
+  document.onkeyup = function(event) {
+    var userGuess = event.key;
+    var doubles = alreadyGuessed.includes(userGuess);
+    var validKeys = alphabet.includes(userGuess);
+    if (doubles) {
+      return;
+    } else if (!validKeys) {
+      return;
+    } else {
+      game();
     }
+
+    function game() {
+      if (userGuess === letter) {
+        wins++;
+        document.querySelector("#winning").innerHTML = wins;
+        restart();
+      } else {
+        lives--;
+        document.querySelector("#guesses").innerHTML = lives;
+        alreadyGuessed.push(userGuess);
+        document.querySelector("#guessed").innerHTML = alreadyGuessed.join(
+          ", "
+        );
+      }
+      if (lives === 0) {
+        losses++;
+        document.querySelector("#losing").innerHTML = losses;
+        restart();
+      }
+    }
+  };
 }
 
 
-// when lives at 0 -- game restarts.
